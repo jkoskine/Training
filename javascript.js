ecddbf6bde5cd54e3847c24af2130fcb8fae64fc
jkoskine <juhavkoskinen@gmail.com>
2022-02-01 21:47:09 +0200

    ok?

--- javascript.js
+++ javascript.js
@@ -1,17 +1,6 @@
+
 console.log("some output");
 console.log(1+1);
 
 const friends = ["Mikko", "Pate", "Hilma", "Xavier"];
-console.log(friends);
-
-friends[1] = "Janne";
-console.log(friends);
-
-friends.push("Jormas");
-console.log(friends);
-
-friends.push("Keijo");
-console.log(friends);
-
-friends.push("Barbababa");
 console.log(friends);

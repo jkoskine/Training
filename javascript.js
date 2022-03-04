c8c46d9aede34197e6c73d4745a06a6afe227af3
jkoskine <juhavkoskinen@gmail.com>
2022-03-04 20:37:23 +0200

    RH korjaus

--- javascript.js
+++ javascript.js
@@ -5,5 +5,5 @@ console.log(1+1);
 const friends = ["Mikko", "Pate", "Hilma", "Xavier"];
 console.log(friends);
 
-const friends2 = ["Mikko", "matti, "Hilma", "Xavier"];
-console.log(friends);
+const friends2 = ["Mikko", "matti", "Hilma", "Xavier"];
+console.log(friends2);
